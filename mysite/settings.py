cbf2e4af26dfa3aa424f4f659f907143921f0077
Saqib <saqibmehmood199@gmail.com>
2020-08-21 22:35:24 +0500

    push

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'hw65ui0c@hcxcuvm^0l++0c*y2ps@f735(mo==5yf85%1z8bn+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['floating-anchorage-45637.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
