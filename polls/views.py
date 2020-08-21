f517d1b11d9a68a06b226bfa3239e49f8d601e18
Saqib <saqibmehmood199@gmail.com>
2020-08-21 15:13:52 +0500

    mysite to git

---
+++ polls/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+# Create your views here.
+from .models import Question
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
